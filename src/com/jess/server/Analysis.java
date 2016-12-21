c904e12c72cfdc670eb5280cb0a196af70ce29f6
Benjamin <benjamin.gonzalez@unima.com.mx>
2016-12-21 06:22:34 +0000

    servidor Jess diagnostico terminado

--- src/com/jess/server/Analysis.java
+++ src/com/jess/server/Analysis.java
@@ -4,6 +4,7 @@ package com.jess.server;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.StringWriter;
 
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
@@ -36,69 +37,44 @@ public class Analysis {
 	
 
 	
-	 static boolean getResponse(String s) { return s.equals("si") || s.equals("s"); }
+	 static boolean getResponse(char c) {
+		 return c == 's'; 
+		 }
 	
   
   @Path("analysis")
   @POST
   @Consumes("application/json ")
-  public Response sayPlainTextHello(String string) throws Exception {
+  @Produces( MediaType.TEXT_PLAIN )
+  public String sayPlainTextHello(String checkBoxes) throws Exception {
 	  
    
-	 
-		  Rete r = new Rete();
-	        r.eval("(reset)");     
-	        r.eval("(defrule meningitis (sin vomito) (sin nausea) (sin insomnia) => (printout t \"\ntiene meningitis\"))");
-	        r.eval("(defrule tubercolosis (sin fiebre) (sin dolordecabeza) (sin insomnia) => (printout t \"\ntiene tubercolosis\"))");
-	        r.eval("(defrule sarna (sin prurito) (sin manchasenlapiel) (sin fiebre) => (printout t \"\ntiene sarna\"))");
-	        r.eval("(defrule salmonela (sin vomito) (sin nausea) (sin diarrea) => (printout t \"\ntiene salmonela\"))");     
-	         
-	     
-	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	         
-	        System.out.println("\nDomanda 1:\nSe tiene vomito? (s/n)");     
-	            
-	        if (getResponse(Character.toString(string.charAt(0)))) r.eval("(assert (sin vomito))");
-	         
-	        System.out.println("\nDomanda 2:\nSe tiene nausea? (s/n)");     
-	      
-	        if (getResponse(Character.toString(string.charAt(1)))) r.eval("(assert (sin nausea))");
-	         
-	        System.out.println("\nDomanda 3:\nSe tiene insomnia? (s/n)");       
-	              
-	        if (getResponse(Character.toString(string.charAt(2)))) r.eval("(assert (sin insomnia))");
-	         
-	        System.out.println("\nDomanda 4:\nSe tiene fiebre? (s/n)");     
-	              
-	        if (getResponse(Character.toString(string.charAt(3)))) r.eval("(assert (sin fiebre))");
-	         
-	        System.out.println("\nDomanda 5:\nSe tiene dolor de cabeza? (s/n)");        
-	            
-	        if (getResponse(Character.toString(string.charAt(4)))) r.eval("(assert (sin dolordecabeza))");
-	         
-	        System.out.println("\nDomanda 6:\nSe tiene prurito? (s/n)");        
-	           
-	        if (getResponse(Character.toString(string.charAt(5)))) r.eval("(assert (sin prurito))");
-	             
-	        System.out.println("\nDomanda 7:\nSe tiene manchas en la piel? (s/n)");     
-	          
-	        if (getResponse(Character.toString(string.charAt(6)))) r.eval("(assert (sin manchasenlapiel))");
-	             
-	        System.out.println("\nDomanda 8:\nSe tiene diarrea? (s/n)");        
-	            
-	        if (getResponse(Character.toString(string.charAt(7)))) r.eval("(assert (sin diarrea))");
-	         
-	        System.out.println("\n ----- \n Las sintomas son:\n");      
-	        r.eval("(facts)");  
-	                     
-	        System.out.println("\n ----- \n Analisis del experto:\n");      
-	    
-	        
-	        String a=  r.eval("(run)").toString();
-	         
-  
-	  
-	  return Response.status(200).build();
+	  Rete r = new Rete();
+      r.eval("(reset)");
+               
+      r.eval("(defrule meningitis (sin vomito) (sin nausea) (sin insomnia) => (printout t \"tiene meningitis\n\"))");
+      r.eval("(defrule tubercolosis (sin fiebre) (sin dolordecabeza) (sin insomnia) => (printout t \"tiene tubercolosis\n\"))");
+      r.eval("(defrule sarna (sin prurito) (sin manchasenlapiel) (sin fiebre) => (printout t \"tiene sarna\n\"))");
+      r.eval("(defrule salmonela (sin vomito) (sin nausea) (sin diarrea) => (printout t \"tiene salmonela\n\"))");     
+       
+      StringWriter sw = new StringWriter();
+      // Connect the "t" router to the StringWriter
+      r.addOutputRouter("t", sw);
+       
+      char[] res = checkBoxes.toCharArray();
+      if (getResponse(res[0])) r.eval("(assert (sin vomito))");
+      if (getResponse(res[1])) r.eval("(assert (sin nausea))");
+      if (getResponse(res[2])) r.eval("(assert (sin insomnia))");
+      if (getResponse(res[3])) r.eval("(assert (sin fiebre))");
+      if (getResponse(res[4])) r.eval("(assert (sin dolordecabeza))");
+      if (getResponse(res[5])) r.eval("(assert (sin prurito))");
+      if (getResponse(res[6])) r.eval("(assert (sin manchasenlapiel))");
+      if (getResponse(res[7])) r.eval("(assert (sin diarrea))");
+               
+      r.eval("(run)");
+       
+      return (sw.toString().length() > 0 ? sw.toString() : "Usted esta perfectamente!\n"); 
+ 
   }
 
  
